9f540a9f2dc9e8ab1db4059b37889aa79f7f0e82
EstherImamun <imams1234.et@gmail.com>
2023-01-21 01:02:26 +0100

    Loop2

---
+++ src/com/syntax/review4/HomeWorkNestedLoop.java
@@ -0,0 +1,21 @@
+package com.syntax.review4;
+
+public class HomeWorkNestedLoop {
+    public static void main(String[] args) {
+        for (int a = 0; a <= 2; a++) {
+            for (int b = 0; b <= 3; b++) {
+                for (int c = 00; c <=5; c++) {
+                    for (int d = 0; d <=9; d++) {
+
+                        System.out.println(a + " " + b + ":" + c + " " + d);
+                    }
+                    System.out.println();
+                }
+
+            }
+
+
+        }
+    }
+}
+
